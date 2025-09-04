43c75a7b4bef3659af2055b17247802934804884
romelmar alejandrino <romelmar.alejandrino@gmail.com>
2025-09-04 20:50:44 +0800

    add login endpoint to api.php

--- be_redzone/routes/api.php
+++ be_redzone/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\SubscriberController;
 use App\Http\Controllers\SubscriptionController;
 use Illuminate\Http\Request;
@@ -8,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/subscribers', [SubscriberController::class, 'getSubscribers']);
 Route::get('/subscribers-with-dues', [SubscriberController::class, 'getSubscribersWithDues']);
