cf3d9611abaf836f3e692c13b711cc35c560b8b9
unknown <romelmar.alejandrino@gmail.com>
2026-02-08 12:36:05 +0800

    fix 419 error

--- be_redzone/app/Providers/AppServiceProvider.php
+++ be_redzone/app/Providers/AppServiceProvider.php
@@ -10,10 +10,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+   /**
+ * Register any application services.
+ */
+public function register(): void
+{
+    if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+        $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        $this->app->register(TelescopeServiceProvider::class);
     }
+}
 
     /**
      * Bootstrap any application services.
--- fe_redzone/src/stores/auth.js
+++ fe_redzone/src/stores/auth.js
@@ -6,6 +6,8 @@ import { defineStore } from 'pinia'
 // axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
