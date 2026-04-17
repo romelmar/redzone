8da81afe2aabd54cf1302b855d27d474117c5f8a
romelmar <romelmar.alejandrino@gmail.com>
2026-04-17 22:07:31 +0800

    cors 2

--- be_redzone/config/cors.php
+++ be_redzone/config/cors.php
@@ -3,8 +3,9 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173',
-    //  'https://redzone.rosnel-partnership.com'
+    'allowed_origins' => [
+        // 'http://localhost:5173',
+     'https://redzone.rosnel-partnership.com'
      ], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
